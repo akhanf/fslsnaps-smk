0757b67d1c745c03ac86fa3e23f9e796780ae6fd
Ali Khan <alik@robarts.ca>
2022-08-26 16:43:11 -0400

    branch for new figure

--- 00_runAll.sh
+++ 00_runAll.sh
@@ -11,4 +11,4 @@ do
     snakemake --cores all --configfile config/config_${method}.yml --rerun-incomplete --keep-going --notemp
 done
 
-
+montage results/sub-7607477/sub-7607477_hemi-R_desc-*_opacity-0_method-hippunfoldT1_snap.png results/sub-7607477/sub-7607477_hemi-[rR]*opacity-100_method-hippunfoldT1_snap.png  results/sub-7607477/sub-7607477_hemi-[rR]*opacity-100_method-ashs_snap.png results/sub-7607477/sub-7607477_hemi-[rR]*opacity-100_method-freesurfer_snap.png -geometry '1x1+0+0<' -tile 4x4 results/sub-7607477_figuremontage.png
